4356b500ca8e18638d75da79993f41fcef730dae
awinters81 <alexandria.winters81@gmail.com>
2023-01-15 13:13:01 -0500

    starting js

---
+++ script.js
@@ -0,0 +1,19 @@
+//Variables will need to be created to pull from the API
+
+var weatherApi = 'https://api.openweathermap.org';
+var weatherKey = '';
+
+//And I have to create variables to reference the HTML elements
+
+var search = document.querySelector('#search')
+var input = document.querySelector('#input')
+var current = document.querySelector('#current')
+var forecast = document.querySelector('#forecast')
+
+
+// function startSearchCity(){
+
+// }
+
+// Well as always, I know I need a listener, which I'll have to create a function for.
+//searchCity.addEventListener('click', startSearchCity)
